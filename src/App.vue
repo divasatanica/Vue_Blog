d4d4687fc7e4ba0f1c2115f79f1c8cb892f90741
divasatanica <jayhebes@vip.qq.com>
2017-08-16 20:33:41 +0800

    mes:initial upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <blogheader></blogheader>
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+import blogheader from '@/components/header'
+
+export default {
+  name: 'app',
+  components: {
+    blogheader
+  }
+}
+</script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    background: {
+      color: rgb(200,200,200);
+    }
+  }
+  html {
+    font: {
+      size: 14px;
+    }
+  }
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1 class="cusFont">Coma's Blog</h1>
+    <p class="cusFont">壮士一去不复返</p>
+    <button class="login">{{ msg }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '登录',
+      isLoggedIn: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  div {
+    padding: {
+      top: 5%;
+      left: 5%;
+    }
+    width: 75%;
+    margin: 0 auto;
+  }
+  h1 {
+    margin: 0 !important;
+  }
+  .cusFont {
+    font: {
+      weight: lighter;
+      family: Microsoft Yahei
+    }
+  }
+  .login {
+    position: relative;
+    bottom: 44px;
+    right: 5%;
+    float: right;
+    cursor: pointer;
+    transition: all 0.3s ease;
+  }
+  .login:hover {
+    color: white;
+    background-color: rgb(20,20,20);
+  }
+  button {
+    border: none;
+    border-radius: 3px;
+    padding: 5px 8px;
+    font: {
+      size: 1.2rem;
+    }
+  }
+</style>
+
