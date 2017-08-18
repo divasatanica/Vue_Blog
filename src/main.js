ac865b855a5ae522139e13df4571e27e2a02f793
divasatanica <jayhebes@vip.qq.com>
2017-08-19 00:00:22 +0800

    feat: 引入element-ui、vue-resource

--- src/main.js
+++ src/main.js
@@ -3,13 +3,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  http: {
+    root: '/root',
+    headers: {
+      Authorization: 'Basic YXBpOnBhc3N3b3Jk'
+    }
+  }
 })
