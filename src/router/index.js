152f5ded964cdb21469394dd5c8f200ceb925772
divasatanica <jayhebes@vip.qq.com>
2017-08-20 00:20:19 +0800

    feat: 添加vue-router路由配置文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
-/* import Vue from 'vue'
+import Vue from 'vue'
 import Router from 'vue-router'
-import Header from '@/components/Hello'
+
+/* components */
+
+import blogHeader from '@/components/header'
+import navBar from '@/components/nav-bar'
+import passageDisplay from '@/components/passage-display'
+import indexPassage from '@/components/index-passage-container'
+import login from '@/components/login-message'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      components: {
+        default: indexPassage,
+        login: login
+      }
+    },
+    {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'passages',
+      component: indexPassage
     }
   ]
-}) */
+})
