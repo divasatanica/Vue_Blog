d807ff580d64f7105791d736c1757a005355b045
divasatanica <jayhebes@vip.qq.com>
2017-09-09 21:56:43 +0800

    feat: 根据标签或日期区间进行文章的筛选

--- src/components/index-passage-container.vue
+++ src/components/index-passage-container.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="index-passage-container">
-    <div class="passage-container-topbar"></div>
+    <div class="passage-container-topbar"><span class="return-to" v-if="mode !== 1" @click="req()">退出筛选结果</span></div>
     <div class="passage-func-zone">
       <div class="passages">
-        <single v-for="(item, index) in articles" :key="item._id" :article="item" ></single>
+        <single v-for="(item, index) in articles" :key="item._id" :article="item" @filtertag="filterByTag"></single>
       </div>
-      <div class="side-bar"></div>
+      <div class="side-bar">
+        <div class="filter-date">
+          <el-date-picker
+            v-model="date"
+            type="daterange"
+            placeholder="选择日期范围">
+          </el-date-picker>
+          <button class="filter-date-btn" @click="filterByDate()">筛选</button>
+        </div>
+      </div>
     </div>
+    <el-pagination
+      small
+      layout="prev, pager, next"
+      :total="total" :page-size="5" v-on:current-change="moreArticle">
+    </el-pagination>
   </div>
 </template>
 
@@ -15,21 +29,87 @@ import single from '@/components/index-passage-single'
 export default {
   data() {
     return {
-      articles:[]
+      articles:[],
+      page: 1,
+      total: 0,
+      date: '',
+      mode: 1, //1为普通模式，2为日期筛选，3为标签筛选,
+      tag2filter: ''
     }
   },
   methods: {
     req() {
-      this.$http.get('http://comacc.top/article/newest/?offset=0').then(response => {
-        for(let i in response.data) {
-          this.articles.push(response.data[i]);
-        }
-      }, (err) => {
-        console.error(err);
-      })
+        this.mode = 1;
+        this.date = '';
+        this.$http.get('http://comacc.top/article/newest/0').then(this.renderPassages, (err) => {
+          throw err;
+        }).catch(this.alertTheError)
     },
-    checkPassage(index) {
-
+    moreArticle(param) {
+        if(this.mode == 1) {
+          this.$http.get(`http://comacc.top/article/newest/${param-1}`).then(this.renderPassages, err => {
+            throw err;
+          }).catch(this.alertTheError)
+        }
+        else if(this.mode == 2) {
+          this.filterByDate(param);
+        }
+        else if(this.mode == 3) {
+          this.filterByTag(this.tag2filter, param);
+        }
+    },
+    filterByDate(param = 1) {
+      const dateParam = this.generate();
+      const startDate = dateParam.startDate;
+      const endDate = dateParam.endDate;
+      this.mode = 2;
+      this.$http.post('http://comacc.top/article/filtering', {
+        startDate,
+        endDate,
+        page: param
+      }).then(this.renderPassages, err => {
+        throw err;
+      }).catch(this.alertTheError);
+    },
+    filterByTag(tag, param = 1) {
+      this.tag2filter = tag;
+      this.mode = 3;
+      this.$http.post('http://comacc.top/article/filtering', {
+        tag,
+        page: param
+      }).then(this.renderPassages, err => {
+        throw err;
+      }).catch(this.alertTheError);
+    },
+    generate(){
+      if(!this.date || !this.date[0] || !this.date[1]) {return;}
+      let startDate = this.date[0];
+      let endDate = this.date[1];
+      startDate = `${startDate.getFullYear()}${((startDate.getMonth()+1)<10)?`0${startDate.getMonth()+1}`:`${startDate.getMonth()}`}${(startDate.getDate()<10)?`0${startDate.getDate()}`:`${startDate.getDate()}`}000`;
+      endDate = `${endDate.getFullYear()}${((endDate.getMonth()+1)<10)?`0${endDate.getMonth()+1}`:`${endDate.getMonth()}`}${(endDate.getDate()<10)?`0${endDate.getDate()}`:`${endDate.getDate()}`}000`;
+      return {startDate, endDate};
+    },
+    renderPassages(res) {
+      if(res.data.code == 0) {
+        this.articles = [];
+        this.total = 1;
+      }
+      else {
+        let article = res.data.article;
+        this.total = res.data.length;
+        this.articles = [];
+        for(let i in article) {
+          this.articles.push(article[i])
+        }
+      }
+    },
+    alertTheError(e) {
+      console.log(e);
+      this.$message({
+              message: '网络出现问题π_π 请稍后再试',
+              type: 'error',
+              duration: 1500
+          })
     }
   },
   components: {
@@ -43,11 +123,11 @@ export default {
 
 <style lang="scss" scoped>
   .index-passage-container {
-    width: 1100px;
+    width: 90%;
     margin: 0 auto;
     margin-top: 50px;
     margin-bottom: 50px;
-    box-shadow: 5px 9px 10px 0 rgba(40,40,40,0.4);
+    overflow: hidden;
   }
   .passage-container-topbar {
     width: 100%;
@@ -59,17 +139,65 @@ export default {
       top-left-radius: 5px;
       top-right-radius: 5px;
     }
+    .return-to {
+      display: inline-block;
+      height: 40px;
+      line-height: 40px;
+      margin-left: 3rem;
+      padding: {
+        left: .5rem;
+        right: .5rem;
+      }
+      color: #fff;
+      transition: all 0.2s ease;
+      cursor: pointer;
+      &:hover {
+        background-color: rgba(200,200,200,0.3);
+      }
+    }
   }
   .passage-func-zone {
     display: flex;
+    height: 510px;
   }
   .passages {
     width: 60%;
     border: none;
+    height: 510px;
+    background-color: rgba(195,190,186,0.7);
   }
   .side-bar {
     width: 40%;
     border: none;
     background-color: rgba(195,190,186,0.7);
+    border: {
+      left: 1px solid #a0a0a0;
+    }
+  }
+  .el-date-editor {
+    margin-left: 2rem;
+  }
+  .filter-date {
+    padding-left: 1rem;
+    margin-top: 25px;
+  }
+  .filter-date-btn {
+    width: 6rem;
+    height: 2.2rem;
+    margin-left: 2rem;
+    margin-right: 1rem;
+    color: #fff;
+    font-size: 1rem;
+    transition: all 0.2s ease;
+    background: {
+      color: #808080;
+    }
+    border: 1px solid #808080;
+    &:hover {
+      border: 1px solid #a0a0a0;
+    }
+    &:active {
+      transform: translateY(3px);
+    }
   }
 </style>
--- src/components/index-passage-single.vue
+++ src/components/index-passage-single.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <span><h1 v-on:click="detail(index)"><router-link :to="nav">{{article.header}}</router-link></h1></span><span class="time">{{article.timeStamp | geneTime}} {{article.clock}}</span>
+    <span><h1><router-link :to="nav">{{article.header}}</router-link></h1></span><span class="time">{{article.timeStamp | geneTime}} {{article.clock}}</span>
     <div class="tags"><span class="tag" v-for="(item, index) in article.tag" v-bind:key="item.timeStamp" v-on:click="filterTags(item)">#{{item}}</span></div>
   </div>
 </template>
@@ -21,11 +21,8 @@ export default {
     }
   },
   methods: {
-    detail(index) {
-      console.log(index);
-    },
     filterTags(tag) {
-      console.log(tag);
+      this.$emit('filtertag', tag);
     }
   }
 }
@@ -46,6 +43,8 @@ export default {
     color: #000;
   }
   .container {
+    min-width: 20rem;
+    height: 82px;
     padding: 10px 16px;
     background-color: rgba(189,184,180,0.7);
   }
