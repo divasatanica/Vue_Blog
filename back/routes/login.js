2eaa71d75f310c1d5078293b72b5b36d9e2c011d
divasatanica <jayhebes@vip.qq.com>
2017-10-22 11:01:34 +0800

    fix: 去除无用响应，改为提示具体错误信息。

--- back/routes/login.js
+++ back/routes/login.js
@@ -14,26 +14,26 @@ router.post('/', (req, res) => {
         result.forEach((rec) => {
           if(rec.name == req.cookies.name) {
             res.header("Access-Control-Allow-Credentials","true");
-            res.sendStatus(200);
+            res.send(JSON.stringify({username: req.cookies.name, message: 'Success'}));
           }
         })
       }, {}, "user")
     })
   }
   else if(!param.name) {
-    res.sendStatus(400);
+    res.send(JSON.stringify({message: 'Invalid Request'}));
   }
   else {
     let date = new Date();
     mongo.connect(DB_CONN_STR, (err, db) => {
       dbhandle.findData(db, (result) => {
         if(result[0].password !== param.password) {
-          res.sendStatus(403);
+          res.send(JSON.stringify({message: 'Wrong Password'}));
         }
         else {
           res.header("Access-Control-Allow-Credentials","true");
           res.cookie("name", param.name, {httpOnly: false, maxAge: 7*24*3600*1000, expires: new Date(date.getTime()+7*24*3600*1000)});
-          res.sendStatus(200);
+          res.sendStatus(JSON.stringify({username: param.name}));
         }
       }, whereStr, "user")
     });
