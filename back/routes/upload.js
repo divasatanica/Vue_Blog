1b6d570486d112e0eadb8c78342936858dfbeec0
divasatanica <jayhebes@vip.qq.com>
2017-10-22 11:02:09 +0800

    fix: Promise.race设置全体resolve结果

--- back/routes/upload.js
+++ back/routes/upload.js
@@ -61,7 +61,8 @@ router.post('/qiniu', (req, res) => {
   // multipart包解析form-data数据
   form.parse(req, (err, fields, files) => {
     if(err) { throw err;}
-    let fileArr = files.file[0];
+    let fileArr = files.file;
+    const _promise = [];
     if(!fileArr) {
       res.header("Access-Control-Allow-Origin", "*");
       res.send(JSON.stringify({message: 'No file specified'}));
@@ -69,24 +70,34 @@ router.post('/qiniu', (req, res) => {
     }
     // 生成七牛云上传token, 有效期为1小时
     const token = _genToken('gRg3qjpihTLupXOxG4NKwdacjguTiA4yRIF6DxGz', 'yMaUGpR1JZbBsSTFX64Ig4sMCGabz153YUIqBdxs');
-    formUploader.putFile(token, undefined, fileArr.path, putExtra, function(respErr,
-      respBody, respInfo) {
-      if (respErr) {
-        throw respErr;
-      }
-      if (respInfo.statusCode == 200) {
-        console.log(respBody);
-        fs.unlink(fileArr.path, (err) => {
-          if(err) { throw err;}
-          console.log(`$THE FILE ${fileArr.path} HAS BEEN DELETED`);
-        });
-        res.send(JSON.stringify(respBody));
-      } else {
-        console.log(respInfo.statusCode);
-        console.log(respBody);
-        res.send(JSON.stringify(respBody));
-      }
-    });
+    // 使用Promise.all应对多张图片同时上传的情况，上传完毕后一次性返回结果
+    for(let i = 0, len = fileArr.length;i < len;i ++) {
+      _promise.push((function() {
+        return new Promise(function(resolve, reject) {
+          formUploader.putFile(token, undefined, fileArr[i].path, putExtra, function(respErr,
+            respBody, respInfo) {
+            if (respErr) {
+              throw respErr;
+            }
+            if (respInfo.statusCode == 200) {
+              resolve(respBody);
+            } else {
+              reject(respBody);
+            }
+          });
+        })
+      })());
+      // 上传完成后删除缓存中的源文件
+      fs.unlink(fileArr[i].path, (err) => {
+        if(err) { throw err;}
+        console.log(`$THE FILE ${fileArr[i].path} HAS BEEN DELETED`);
+      });
+    }
+    // 这里可以改成Promise.race以防止有某个请求失败时导致不返回任何结果
+    Promise.race(_promise).then(function(result) {
+      result.url = `http://cdn.comacc.top/${result.hash}`
+      res.send(JSON.stringify({message: 'Success', files: result}));
+    }).catch(function(e){ throw new Error(e);});
   });
   function _genToken(accessToken, secretToken) {
     const _bucket = 'coma'
